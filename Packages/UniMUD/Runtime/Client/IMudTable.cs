3758ea3ad26162ba8c5863837ce18797e3e89bb6
Neilson K-S <neilson.ks@gmail.com>
2023-07-12 12:10:32 -0700

    imudtable more abstracts

--- Packages/UniMUD/Runtime/Client/IMudTable.cs
+++ Packages/UniMUD/Runtime/Client/IMudTable.cs
@@ -1,4 +1,8 @@
 #nullable enable
+using mud.Network.schemas;
+using mud.Unity;
+using Property = System.Collections.Generic.Dictionary<string, object>;
+using System.Collections.Generic;
 
 namespace mud.Client
 {
@@ -7,6 +11,47 @@ namespace mud.Client
         public abstract void SetValues(params object[] functionParameters);
         public abstract object[] GetValues();
         public abstract IMudTable? GetTableValue(string key);
+        public readonly TableId TableId;
+
+        public static IMudTable? GetValue<T>(string key) where T : IMudTable, new()
+        {
+            var table = new T();
+            var query = new Query()
+                .Find("?value", "?attribute")
+                .Where(table.TableId.ToString(), key, "?attribute", "?value");
+            var result = NetworkManager.Instance.ds.Query(query);
+            var hasValues = table.SetValues(result);
+            return hasValues ? table : null;
+
+        }
+
+        public abstract bool SetValues(IEnumerable<Property> result);
+        
+        // {
+
+        //     var hasValues = false;
+
+        //     foreach (var record in result)
+        //     {
+        //         var attribute = record["attribute"].ToString();
+        //         var value = record["value"];
+
+        //         switch (attribute)
+        //         {
+        //             case "x":
+        //                 var xValue = (long)value;
+        //                 table.x = xValue;
+        //                 hasValues = true;
+        //                 break;
+        //             case "y":
+        //                 var yValue = (long)value;
+        //                 table.y = yValue;
+        //                 hasValues = true;
+        //                 break;
+        //         }
+        //     }
+
+        // }
 
     }
 }
