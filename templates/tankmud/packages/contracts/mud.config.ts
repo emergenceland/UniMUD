8e24fe806184c4ec9a908648b489c6f66184a254
Neilson <neilson.ks@gmail.com>
2023-11-20 17:25:33 -0800

    Update mud.config.ts

--- templates/tankmud/packages/contracts/mud.config.ts
+++ templates/tankmud/packages/contracts/mud.config.ts
@@ -1,26 +1,13 @@
 import { mudConfig, resolveTableId } from "@latticexyz/world/register";
 
 export default mudConfig({
+
   tables: {
-    Position: {
-      valueSchema: {
-        x: "int32",
-        y: "int32",
-      },
-    },
-    Health: {
-      valueSchema: {
-        value: "int32",
-      },
-    },
-    Player: "bool",
-    Damage: "int32",
+
   },
+
   modules: [
-    {
-      name: "KeysWithValueModule",
-      root: true,
-      args: [resolveTableId("Position")],
-    },
+ 
   ],
+  
 });
